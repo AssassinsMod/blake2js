75d22ed71d3911b75ad03e98d69930e44b290f38
AssassinsMod <assassinsmod@live.it>
2016-05-12 15:59:51 +0200

    Testing Blake2b class

--- test/index.js
+++ test/index.js
@@ -2,6 +2,25 @@
 
 'use strict';
 
-const api = require('bindings')('hook');
+const EH = require('segfault-handler');
+EH.registerHandler('crash.log');
 
-console.log(api.Blake2bInit());
+try {
+	const blake2 = require('bindings')('addon');
+
+	// Blake2b
+	const blake2b = blake2.createHash('blake2b');
+	return;
+	/*
+		const blake2b_key = blake2.createHash('blake2b', {
+			key: 'key',
+			length: 64
+		});
+	//*/
+
+	blake2b.update(new Buffer('00FF', 'hex'))
+
+	console.log(blake2b.digest().toString('hex'));
+} catch (err) {
+	console.log("ERROR:", err);
+}
